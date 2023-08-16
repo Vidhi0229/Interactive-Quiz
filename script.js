3318a59d5271e82ca89a8f26ff8e977e60a72bab
Vidhi <srivastavavidhi29@gmail.com>
2023-08-16 12:22:44 +0530

    Added js to show result

---
+++ script.js
@@ -0,0 +1,19 @@
+const correct = ['A', 'C', 'D', 'C'];
+
+let result = document.querySelector(".result");
+
+let form = document.querySelector(".quiz-form");
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let score = 0;
+    const ans = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+
+    ans.forEach((answer, index) => {
+        if(answer === correct[index]){
+            score += 25;
+        }
+    })
+    result.querySelector("span").textContent = `${score}%`;
+    result.removeAttribute('class', 'd-none'); //or we can use result.classList.remove("d.none")
+})
